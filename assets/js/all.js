a18d931ffbb75195ddf0c58b4726da817666bd86
Hauke Bartsch <hbartsch@ucsd.edu>
2018-12-21 09:33:24 -0800

    use default data dictionary

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,10 @@
 jQuery(documents).ready(function() {
 
+    // add a test dictionary to the input field
+    jQuery('#input').val('https://ndar.nih.gov/api/datadictionary/v2/datastructure/cct01/csv');
+
     jQuery('#input').on('change', function() {
-        
+
         console.log("page loaded...");
     });
 });
